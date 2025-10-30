360895eb0bd9b3b35382fb525b032b46aee50fd4
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2025-10-30 09:42:46 -0300

    j

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click', function(){
-        2
-    })
+        const atual = document.querySelector('.ativo')
+const proximoPasso = "passo-" + this.getAttribute('data-proximo')
+atual.classList.remove('ativo')
+document.getElementById(proximoPasso).classList.add('ativo')
+})
 }
 
 )
